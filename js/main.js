580a5703dada36d2c77bcfbbc26649b6bbb2a684
Shawn O'Neill <soneill83@gmail.com>
2020-06-14 22:35:28 -0700

    Added docs link, cache bust JS

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//Docs: https://developers.google.com/chart/interactive/docs/gallery/linechart
 google.charts.load('current', {'packages':['corechart']});
 google.charts.setOnLoadCallback(drawChart);
 
